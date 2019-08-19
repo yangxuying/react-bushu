8017c9f6ceb51925ba5876590f31762bf097b7f9
yangxuying <2710343126@qq.com>
2019-08-19 15:45:27 +0800

    react

---
+++ precache-manifest.9005152ed93f8cc219e6494e4b63eed1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223492b5066782536137eb0ef840e559",
+    "url": "/react-bushu/index.html"
+  },
+  {
+    "revision": "eda624ba9a58367a0a23",
+    "url": "/react-bushu/static/css/main.685dea8f.chunk.css"
+  },
+  {
+    "revision": "0681e5bb692bcc9416aa",
+    "url": "/react-bushu/static/js/2.ed64165c.chunk.js"
+  },
+  {
+    "revision": "eda624ba9a58367a0a23",
+    "url": "/react-bushu/static/js/main.17fca6c3.chunk.js"
+  },
+  {
+    "revision": "b1ea01805cda07faae84",
+    "url": "/react-bushu/static/js/runtime~main.d25e4b4b.js"
+  }
+]);
